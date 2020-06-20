877e8ee58124b7a76be42321831818c01b6bdc01
Jean Borges <jeanborges946@gmail.com>
2020-06-20 18:14:20 -0300

    funçao adiçao de dados

--- manage_db.py
+++ manage_db.py
@@ -2,17 +2,6 @@ import sqlite3
 
 con = sqlite3.connect('base.db')
 cur = con.cursor() 
- 
-# HardCode insertion
-# sql = """
-#   INSERT INTO users(name, phone, email)
-#   VALUES('Jean', '3595333961184', 'jeanborges946@gmail.com' ) """
-
-# cur.execute(sql)
-# con.commit()
-# con.close()
-
-# With a function
 
 def db_insert(name, phone, email):
   return """
